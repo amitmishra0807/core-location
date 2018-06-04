9adec8385da98e841581084afd2b3d467b1d267f
amitmishra0807 <34858434+amitmishra0807@users.noreply.github.com>
2018-06-04 17:12:41 +0530

    Add files via upload

---
+++ MapLocator/MapLocator/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  MapLocator
+//
+//  Created by Malek T. on 9/28/16.
+//  Copyright © 2016 Medigarage Studios LTD. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    var searchController:UISearchController!
+    
+    var annotation:MKAnnotation!
+    
+    
+    
+    var localSearchRequest:MKLocalSearchRequest!
+    
+    var localSearch:MKLocalSearch!
+    var localSearchResponse:MKLocalSearchResponse!
+    var error:NSError!
+    var pointAnnotation:MKPointAnnotation!
+    var pinAnnotationView:MKPinAnnotationView!
+    
+    @IBOutlet var mapView: MKMapView!
+
+    
+    
+    @IBAction func showSearchBar(_ sender: AnyObject) {
+        
+        searchController = UISearchController(searchResultsController: nil)
+        
+        searchController.hidesNavigationBarDuringPresentation = true
+        
+        self.searchController.searchBar.delegate = self
+        
+        present(searchController, animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        //1
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+        if self.mapView.annotations.count != 0{
+            annotation = self.mapView.annotations[0]
+            self.mapView.removeAnnotation(annotation)
+        }
+        //2
+        localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start { (localSearchResponse, error) -> Void in
+            
+            if localSearchResponse == nil{
+                let alertController = UIAlertController(title: nil, message: "Place Not Found", preferredStyle: UIAlertControllerStyle.alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            //3
+            self.pointAnnotation = MKPointAnnotation()
+            self.pointAnnotation.title = searchBar.text
+            self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude:     localSearchResponse!.boundingRegion.center.longitude)
+            
+            //Interview question
+            
+            // what is the property name of latitude and longitude .
+            
+            self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
+            self.mapView.centerCoordinate = self.pointAnnotation.coordinate
+            self.mapView.addAnnotation(self.pinAnnotationView.annotation!)
+        }
+    }
+}
+
